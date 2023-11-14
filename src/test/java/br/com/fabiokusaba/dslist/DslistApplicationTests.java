2283b00c69f2c6d53a235865d1ceb0c890cadb23
Fabio Kusaba <fabinhokb44@gmail.com>
2023-11-13 21:21:46 -0300

    Project created

---
+++ src/test/java/br/com/fabiokusaba/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiokusaba.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
